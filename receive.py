26c26dc7321eed476826353548e40e5a95928346
kkbaidu <kingsleybaidu99@gmail.com>
2024-04-14 20:11:04 +0000

    first commit

---
+++ receive.py
@@ -0,0 +1,15 @@
+from flask import Flask, request, redirect
+from twilio.twiml.messaging_response import MessagingResponse
+
+app = Flask(__name__)
+
+@app.route("/whatsapp", methods=['GET', 'POST'])
+def whatsapp_reply():
+    response = MessagingResponse()
+
+    response.message("Thank you for contacting KingsAccess")
+
+    return str(response)
+
+if __name__ == "__main__":
+    app.run(port=8080)
---
+++ send.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+from twilio.rest import Client
+
+load_dotenv()
+
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+
+client = Client(account_sid, auth_token)
+
+token = '0d0aa620b5fe4efdfcb8ebfc9e06512a'
+
+from_whatsapp_number = 'whatsapp:+14155238886'
+to_whatsapp_number = 'whatsapp:' + os.environ['MY_PHONE_NUMBER']
+
+client.messages.create(body='Welcome Kingsley, how may I do for you😊?', from_=from_whatsapp_number, to=to_whatsapp_number)
